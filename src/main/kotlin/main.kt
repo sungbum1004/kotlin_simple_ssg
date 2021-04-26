94b5dbdeb36e50953565afaddb4b9a0840fafd77
sungbum1004 <tlsqkfka96@naver.com>
2021-04-26 22:54:57 +0900

    세팅

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun readLineTrim() = readLine()!!.trim()
+
+fun main() {
+    println("== SIMPLE SSG 시작 ==")
+
+    while ( true ) {
+        print("명령어) ")
+        val command = readLineTrim()
+    }
+    println("== SIMPLE SSG 끝 ==")
+}
