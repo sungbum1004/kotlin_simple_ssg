913c9d072f28df660407cbbb121ccc0db8506dd4
sungbum1004 <tlsqkfka96@naver.com>
2021-04-26 23:57:15 +0900

    URI 명령어에서 정보 얻어내기

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,6 +6,32 @@ fun main() {
     while ( true ) {
         print("명령어) ")
         val command = readLineTrim()
+        val commandBits = command.split("?", limit = 2)
+        val url = commandBits[0]
+        val paramStr = commandBits[1]
+        val paramMap = mutableMapOf<String, String>()
+        // paramStr = id=2&title=안녕&body=ㅋㅋㅋ
+        val paramStrBits = paramStr.split("&")
+
+        for ( paramStrBit in paramStrBits ) {
+           val paramStrBitBits = paramStrBit.split("=", limit = 2)
+            val key = paramStrBitBits[0]
+            val value = paramStrBitBits[1]
+
+            paramMap[key] = value
+        }
+
+        when (url) {
+            "/system/exit" -> {
+                println("프로그램이 종료됩니다.")
+                break
+            }
+            "/article/detail" -> {
+                val id = paramMap["id"]!!.toInt()
+
+                println("${id}번 게시물을 선택하였습니다.")
+            }
+        }
     }
     println("== SIMPLE SSG 끝 ==")
 }
