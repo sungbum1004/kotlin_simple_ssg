5ab42ec7e09062d6dd726367959ba0c53d00a6c5
sungbum1004 <tlsqkfka96@naver.com>
2021-04-27 10:59:48 +0900

    Rq 클래스 생성

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,35 +3,24 @@ fun readLineTrim() = readLine()!!.trim()
 fun main() {
     println("== SIMPLE SSG 시작 ==")
 
-    while ( true ) {
+    while (true) {
         print("명령어) ")
         val command = readLineTrim()
-        val commandBits = command.split("?", limit = 2)
-        val url = commandBits[0]
-        val paramStr = commandBits[1]
-        val paramMap = mutableMapOf<String, String>()
-        // paramStr = id=2&title=안녕&body=ㅋㅋㅋ
-        val paramStrBits = paramStr.split("&")
 
-        for ( paramStrBit in paramStrBits ) {
-           val paramStrBitBits = paramStrBit.split("=", limit = 2)
-            val key = paramStrBitBits[0]
-            val value = paramStrBitBits[1]
-
-            paramMap[key] = value
-        }
-
-        when (url) {
-            "/system/exit" -> {
-                println("프로그램이 종료됩니다.")
-                break
-            }
-            "/article/detail" -> {
-                val id = paramMap["id"]!!.toInt()
-
-                println("${id}번 게시물을 선택하였습니다.")
-            }
-        }
+        val rq = Rq(command)
+        println("rq.actionPath : ${rq.actionPath}, rq.queryStr : ${rq.queryStr}")
     }
+
     println("== SIMPLE SSG 끝 ==")
+}
+
+class Rq(command: String) {
+    val actionPath: String
+    val queryStr: String
+
+    init {
+        val commandBits = command.split("?", limit = 2)
+        actionPath = commandBits[0]
+        queryStr = commandBits[1]
+    }
 }
