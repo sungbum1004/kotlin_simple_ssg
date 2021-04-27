09a52258c1716d77f12fe1563da806f50e7188b6
sungbum1004 <tlsqkfka96@naver.com>
2021-04-27 11:22:34 +0900

    Rq 클래스에 getParam 메서드 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,10 +5,12 @@ fun main() {
 
     while (true) {
         print("명령어) ")
-        val command = readLineTrim()
+        val command = readLineTrim() // 입력 : /article/detail?id=1&title=제목1
 
         val rq = Rq(command)
-        println("rq.actionPath : ${rq.actionPath}, rq.queryStr : ${rq.queryStr}")
+
+        println(rq.getStringParam("title") == "제목1") // true
+        println(rq.getIntParam("id") == 1) // true
     }
 
     println("== SIMPLE SSG 끝 ==")
@@ -16,11 +18,50 @@ fun main() {
 
 class Rq(command: String) {
     val actionPath: String
-    val queryStr: String
+    val paramMap: Map<String, String>
 
     init {
         val commandBits = command.split("?", limit = 2)
-        actionPath = commandBits[0]
-        queryStr = commandBits[1]
+
+        actionPath = commandBits[0].trim()
+        val queryStr = if (commandBits.lastIndex == 1 && commandBits[1].isNotEmpty()) {
+            commandBits[1].trim()
+        } else {
+            ""
+        }
+
+        paramMap = if (queryStr.isEmpty()) {
+            mapOf()
+        } else {
+            val paramMapTemp = mutableMapOf<String, String>()
+
+            // queryStr = id=1&body=2&title=3&age
+            val queryStrBits = queryStr.split("&")
+
+            for (queryStrBit in queryStrBits) {
+                // queryStrBit = id=1
+                val queryStrBitBits = queryStrBit.split("=", limit = 2)
+                val paramName = queryStrBitBits[0]
+                val paramValue = if (queryStrBitBits.lastIndex == 1 && queryStrBitBits[1].isNotEmpty()) {
+                    queryStrBitBits[1].trim()
+                } else {
+                    ""
+                }
+
+                if (paramValue.isNotEmpty()) {
+                    paramMapTemp[paramName] = paramValue
+                }
+            }
+
+            paramMapTemp.toMap()
+        }
+    }
+    fun getStringParam(name: String): String {
+        return paramMap[name]!!
+    }
+    fun getIntParam(name: String): Int {
+        return paramMap[name]!!.toInt()
     }
 }
+
+
