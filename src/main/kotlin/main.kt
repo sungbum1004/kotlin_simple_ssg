85ce979455200105cbd7ce30953dfa61810fdb37
sungbum1004 <tlsqkfka96@naver.com>
2021-04-29 10:13:23 +0900

    게시물 리스트에서 작성자명 표시

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -99,10 +99,12 @@ fun main() {
 
                 val filteredArticles = articleRepository.getFilteredArticles(searchKeyword, page, 10)
 
-                println("번호 / 작성날짜 / 갱신날짜 / 제목 / 내용")
+                println("번호 / 작성날짜 / 작성자 / 제목 / 내용")
 
                 for (article in filteredArticles) {
-                    println("${article.id} / ${article.regDate} / ${article.updateDate} / ${article.title}")
+                    val writer = memberRepository.getMemberById(article.memberId)!!
+                    val writerName = writer.nickname
+                    println("${article.id} / ${article.regDate} / ${writerName} / ${article.title}")
                 }
             }
             "/article/detail" -> {
@@ -247,7 +249,7 @@ object memberRepository {
         nickname: String,
         cellphoneNo: String,
         email: String,
-        ): Int {
+    ): Int {
         val id = ++lastId
         val regDate = Util.getNowDateStr()
         val updateDate = Util.getNowDateStr()
@@ -277,7 +279,18 @@ object memberRepository {
             join("user${id}", "user${id}", "홍길동${id}", "사용자${id}", "0101234123${id}", "user${id}@test.com")
         }
     }
+
+    fun getMemberById(id: Int): Member? {
+        for (member in members) {
+            if (member.id == id) {
+                return member
+            }
+        }
+
+        return null
+    }
 }
+
 
 // 회원 관련 끝
 
@@ -319,7 +332,7 @@ object articleRepository {
     }
 
     fun makeTestArticles() {
-        for (id in 1..100) {
+        for (id in 1..20) {
             addArticle(id % 9 + 1, "제목_$id", "내용_$id")
         }
     }
