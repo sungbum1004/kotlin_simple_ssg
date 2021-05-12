bc2416ccfbe49dfc5984bb7c3b22a0886c9e65ba
sungbum1004 <tlsqkfka96@naver.com>
2021-05-12 13:15:16 +0900

    객체를 얼려서(JSON) 파일에 저장

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -2,11 +2,42 @@ import java.io.File
 
 fun main() {
     //testWriteFile();
-    testWriteFile2();
+    //testWriteFile2();
+    testWriteFile3();
 }
 
-fun testWriteFile() {
-    File("test/1.txt").writeText("안녕하세요.")
+data class TestArticle(
+    val id:Int,
+    val title:String,
+    val body:String
+) {
+    fun toJson(): String {
+        var jsonStr = ""
+
+        jsonStr += "{"
+        jsonStr += "\r\n"
+
+        jsonStr += "\t" + """ "id":$id """.trim() + ","
+
+        jsonStr += "\r\n"
+
+        jsonStr += "\t" + """ "title":"$title" """.trim() + ","
+
+        jsonStr += "\r\n"
+
+        jsonStr += "\t" + """ "body":"$body" """.trim()
+
+        jsonStr += "\r\n"
+
+        jsonStr += "}"
+
+        return jsonStr
+    }
+}
+
+fun testWriteFile3() {
+    val testArticle = TestArticle(1, "제목1", "내용1")
+    File("test/3.json").writeText(testArticle.toJson())
 }
 
 fun testWriteFile2() {
@@ -24,6 +55,8 @@ fun testWriteFile2() {
     fileContent += "\t" + """ "body":"$body" """.trim()
     fileContent += "\r\n"
     fileContent += "}"
+}
 
-    File("test/2.txt").writeText(fileContent)
+fun testWriteFile() {
+    File("test/1.txt").writeText("안녕하세요.")
 } 
